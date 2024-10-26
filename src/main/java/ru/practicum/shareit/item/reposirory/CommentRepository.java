36871b05c15afb4e67a85e794727f41b540941f9
VadimShakhvorostov <160205849+VadimShakhvorostov@users.noreply.github.com>
2024-10-27 03:25:49 +0700

    1

--- src/main/java/ru/practicum/shareit/item/reposirory/CommentRepository.java
+++ src/main/java/ru/practicum/shareit/item/reposirory/CommentRepository.java
@@ -3,7 +3,6 @@ package ru.practicum.shareit.item.reposirory;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ru.practicum.shareit.item.model.Comment;
 
-import java.util.Collection;
 import java.util.List;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
