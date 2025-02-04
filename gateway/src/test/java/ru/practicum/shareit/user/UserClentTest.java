47c7ea5697054df5da9f425b5fa54c901a12b36b
VadimShakhvorostov <160205849+VadimShakhvorostov@users.noreply.github.com>
2025-02-05 01:01:06 +0700

    add readne

---
+++ gateway/src/test/java/ru/practicum/shareit/user/UserClentTest.java
@@ -0,0 +1,11 @@
+package ru.practicum.shareit.user;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class UserClentTest {
+
+    @Autowired
+    private UserClient userClient;
+}
